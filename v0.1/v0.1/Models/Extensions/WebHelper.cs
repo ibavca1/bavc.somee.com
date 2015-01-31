c0823ed6fc6b3d0a45354a90edc69545581e2ee2
ibavca <ibavca@gmail.com>
2015-01-31 05:26:14 +0500

    Добавил Хелпер

---
+++ v0.1/v0.1/Models/Extensions/WebHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace v0._1.Models.Extensions
+{
+    public class Helper
+    {
+
+    }
+
+    public class WebPage
+    {
+
+    }
+
+    public class WebContext
+    {
+
+    }
+
+    public class WebRequest
+    {
+
+    }
+}
